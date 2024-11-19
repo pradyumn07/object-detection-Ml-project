5e3745ad6ef68b4303d21bc4cb225a72d0535b4f
Pradyumn Niranjan <pradyumn07niranjan@gmail.com>
2024-11-19 17:08:18 +0530

    first commit

---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# sign-lang > 2024-11-18 5:18pm
+https://universe.roboflow.com/signlang-pfyv8/sign-lang-k5xgu
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = 'signLanguages',
+    version='0.0.0',
+    author='Pradyumn',
+    author_email='pradyumn07niranjan@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
